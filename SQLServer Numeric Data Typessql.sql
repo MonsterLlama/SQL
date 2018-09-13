95106ed995fe836cc6d5545bbb51d204ab0e98d5
MonsterLlama <MonsterLlama@gmail.com>
2018-09-12 22:11:55 -0700

    updated numeric data types Sql file

--- SQLServer Numeric Data Typessql.sql
+++ SQLServer Numeric Data Typessql.sql
@@ -9,5 +9,10 @@
 -- Storage:		
 SELECT CAST(1 AS bit) AS ["Bit Value"], 
 		CAST(0 AS bit) AS ['Bit Value'],
-		CAST(null AS bit) AS [Bit Value];
+		CAST(null AS bit) AS [Bit Value],
 
+-- DateType:	tinyint 
+-- Description: Allows whole numbers from 0 to 255
+-- Storage:		1 byte	
+	CAST(0 As tinyint) AS [tinyint (min)],
+	CAST(255 As tinyint) AS [tinyint (max)];
