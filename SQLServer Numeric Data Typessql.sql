1e5f1698e7ab9d4c99c2fddc10fb8fefaf2ec8fb
MonsterLlama <MonsterLlama@gmail.com>
2018-09-12 21:21:29 -0700

    Adding first SQL file..

---
+++ SQLServer Numeric Data Typessql.sql
@@ -0,0 +1,13 @@
+
+
+-- Sql Server default data types --
+
+-- Numeric Data Types --
+
+-- DateType:	bit 
+-- Description: Integer that can be 0, 1, or NULL
+-- Storage:		
+SELECT CAST(1 AS bit) AS ["Bit Value"], 
+		CAST(0 AS bit) AS ['Bit Value'],
+		CAST(null AS bit) AS [Bit Value];
+
