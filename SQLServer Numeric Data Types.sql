d2b70c2e1528f9371e54347256525efd0460e3da
MonsterLlama <MonsterLlama@gmail.com>
2018-09-12 22:35:06 -0700

    Adding tinyint info

--- SQLServer Numeric Data Types.sql
+++ SQLServer Numeric Data Types.sql
@@ -7,12 +7,15 @@
 -- DateType:	bit 
 -- Description: Integer that can be 0, 1, or NULL
 -- Storage:		
-SELECT CAST(1 AS bit) AS ["Bit Value"], 
-		CAST(0 AS bit) AS ['Bit Value'],
-		CAST(null AS bit) AS [Bit Value],
+SELECT
+	CAST(0 AS bit) AS ["bit (min)"], 
+	CAST(8.8 AS bit) AS ['bit (max)'], -- any non-zero # defaults to 1
+	CAST(null AS bit) AS [bit (null)],
 
+	'' AS [     ],
 -- DateType:	tinyint 
 -- Description: Allows whole numbers from 0 to 255
 -- Storage:		1 byte	
 	CAST(0 As tinyint) AS [tinyint (min)],
-	CAST(255 As tinyint) AS [tinyint (max)];
+	CAST(255 As tinyint) AS [tinyint (max)],
+	'' AS [     ];
